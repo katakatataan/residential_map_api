38c2269adcf66482ba6964b46915e39b82a2c529
anipro <yuki.matsuyama0123@gmail.com>
2018-09-09 18:32:15 +0900

    debug 用のroutingとauth用のroutingを一旦分けました。

---
+++ src/framework/router_for_debug.go
@@ -0,0 +1,59 @@
+package framework
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func routeForDebug(e *echo.Echo) {
+	e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
+		// 認証をつけル時のデバッグ用に作成
+		fmt.Printf("%s\n", reqBody)
+	}))
+	e.GET("/request", func(c echo.Context) error {
+		req := c.Request()
+		format := `
+			<code>
+				Protocol: %s<br>
+				Host: %s<br>
+				Remote Address: %s<br>
+				Method: %s<br>
+				Path: %s<br>
+			</code>
+		`
+		return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, req.RemoteAddr, req.Method, req.URL.Path))
+	})
+	e.GET("/token", getToken)
+}
+
+func getToken(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	if username == "matsu" && password == "yama" {
+		// Create token
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		// Set claims
+		claims := token.Claims.(jwt.MapClaims)
+		claims["name"] = "Yuki Matsuyama"
+		claims["admin"] = true
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, map[string]string{
+			"token": t,
+		})
+	}
+
+	return echo.ErrUnauthorized
+}
