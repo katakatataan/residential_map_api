d9d7ad70f7a1179d1a7b7489fa45863764c3ebd8
shodak <kousuke.shouda@gmail.com>
2018-09-17 14:15:09 +0900

    gatewayにprefcityを追加

--- src/usecase/repository/sqlhandler.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-// ここはduckタイピングいらないからinterfaceを定義しない
-type SqlHandler interface {
-	Get(res interface{}, query string, args ...interface{}) error
-	Find(res interface{}, query string, args ...interface{}) error
-	Insert(query string, args ...interface{}) (interface{}, error)
-	Update(query string, args ...interface{}) (interface{}, error)
-}
