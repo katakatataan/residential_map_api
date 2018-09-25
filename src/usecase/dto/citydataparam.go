f5953fa5440d1f6d4881f266b9583a950a333225
anipro <yuki.matsuyama0123@gmail.com>
2018-09-25 23:50:55 +0900

    一旦simpleなvalidationを作ったのでこれで表側を動かしてvalidationのfixを待つ

--- src/usecase/dto/citydataparam.go
+++ src/usecase/dto/citydataparam.go
@@ -1,7 +1,9 @@
 package dto
 
 type CityDataParamDto struct {
-	From   string `query:"from" validate:"required"`
+	//{TODO} ここでstringを受けていることろをbindする際にunmarshalする
+	From string `query:"from" validate:"required"`
+	//{TODO} ここでstringを受けていることろをbindする際にunmarshalする
 	To     string `query:"to" validate:"required"`
 	PrefId int64  `query:"pref_id"`
 	CityId int64  `query:"city_id"`
