8c65a276c79b828bf245c06f253464d70d59e5ff
anipro <yuki.matsuyama0123@gmail.com>
2018-09-30 16:53:13 +0900

    validationがない状態に戻す

--- src/usecase/dto/param/citydataparam.go
+++ src/usecase/dto/param/citydataparam.go
@@ -5,8 +5,8 @@ type CityDataParamDto struct {
 	//{TODO} ここでstringを受けていることろをbindする際にunmarshalする
 	// From time.Time `query:"from" validate:"required, canbetime"`
 	// To     time.Time `query:"to" validate:"required, canbetime"`
-	From   string `query:"from" validate:"required, canbetime"`
-	To     string `query:"to" validate:"required, canbetime"`
+	From   string `query:"from" validate:"required"`
+	To     string `query:"to" validate:"required"`
 	PrefId int64  `query:"pref_id"`
 	CityId int64  `query:"city_id"`
 }
