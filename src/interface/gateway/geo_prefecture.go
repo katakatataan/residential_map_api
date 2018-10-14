ed9e3fa9e5047258aa7f0ed72102addbe5d2f0fe
katakatataan <yuki.matsuyama0123@gmail.com>
2018-10-14 15:21:08 +0900

    geojsonの修正

--- src/interface/gateway/geo_prefecture.go
+++ src/interface/gateway/geo_prefecture.go
@@ -14,6 +14,9 @@ type GeoPrefecture struct {
 func (cdg *GeoPrefecture) FindByPrefId(mstGeoPrefParam *param.GeoPrefectureDto) ([]entity.MstPrefectureGeojson, error) {
 	var mstGeojson []entity.MstPrefectureGeojson
 	pp.Println(mstGeoPrefParam)
+	// TODO ここを配列がきた時に対応してくれるように修正
+	// 1. sqlxのin演算子を使う
+	// 2. goでstringに展開する
 	q := "SELECT * FROM mst_prefecture_geojson WHERE pref_id in ($1, $2) AND weight = $3"
 	err := cdg.Find(&mstGeojson, q, mstGeoPrefParam.PrefIds[0], mstGeoPrefParam.PrefIds[1], mstGeoPrefParam.Weight)
 	if err != nil {
--- src/interface/gateway/mst_pref_city_gateway.go
+++ src/interface/gateway/mst_pref_city_gateway.go
@@ -1,6 +1,10 @@
 package gateway
 
-import "residential_map_api/src/entity"
+import (
+	"residential_map_api/src/entity"
+
+	"github.com/k0kubun/pp"
+)
 
 type MstPrefCityGateway struct {
 	SqlHandler
@@ -9,6 +13,7 @@ type MstPrefCityGateway struct {
 func (pcg *MstPrefCityGateway) FindAll() (entity.PrefCities, error) {
 	var prefCities entity.PrefCities
 	err := pcg.Find(&prefCities, "SELECT * FROM mst_pref_city ORDER BY id ASC limit 10")
+	pp.Println(prefCities)
 	if err != nil {
 		return entity.PrefCities{}, err
 	}
