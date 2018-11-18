c1a2e7535814b8ed18469261c5487a50a1604154
katakatataan <yuki.matsuyama0123@gmail.com>
2018-11-18 15:01:33 +0900

    単月のランキングロジック修正

--- src/interface/gateway/city_data_gateway.go
+++ src/interface/gateway/city_data_gateway.go
@@ -37,13 +37,13 @@ func (cdg *CityDataGateway) CompareCitiesInSamePrefecture(prefId int, begin stri
 	// TODO ここ今interface作るの面倒なのであとで直す
 	conn, err := sqlx.Connect("postgres", fmt.Sprintf("user=%s password=%s dbname=%s host=127.0.0.1 port=5432 sslmode=disable", os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_NAME")))
 	// rows, err := conn.Queryx("SELECT * FROM city_data WHERE pref_id = $1 AND build_date >= $2 AND build_date < $3 ORDER BY city_id ASC, build_date ASC", prefId, begin, end)
-	rows, err := conn.Query("SELECT id, year, month, residential_use_type_id, construction_type_id, build_type_id, residential_type_id, structure_type_id, pref_id, pref_name,  to_char(build_date,'YYYY-MM') as build_date, city_id, city_name, built_count, total_square_meter FROM city_data WHERE pref_id = $1 AND build_date >= $2 AND build_date < $3 ORDER BY city_id ASC, build_date ASC", prefId, begin, end)
+	rows, err := conn.Query("SELECT id, year, month, residential_use_type_id, construction_type_id, build_type_id, residential_type_id, structure_type_id, pref_id, pref_name,  to_char(build_date,'YYYY-MM') as build_date, city_id, city_name, built_count, total_square_meter, rank() over( partition by date_trunc('month',build_date) order by built_count desc) as monthly_rank FROM city_data WHERE pref_id = $1 AND build_date >= $2 AND build_date < $3 ORDER BY build_date ASC, monthly_rank ASC", prefId, begin, end)
 	type City struct {
 		CityId           int         `db:"city_id" json:"city_id"`
 		CityName         null.String `db:"city_name" json:"city_name"`
 		BuiltCount       int         `db:"built_count" json:"built_count"`
 		TotalSquareMeter int         `db:"total_square_meter" json:"total_square_meter"`
-		MonthlyRank      int         `db:"montyly_rank" json:"monthly_rank"`
+		MonthlyRank      int         `db:"monthly_rank" json:"monthly_rank"`
 	}
 	type Common struct {
 		Id                   int         `db:"id" json:"id"`
@@ -66,7 +66,7 @@ func (cdg *CityDataGateway) CompareCitiesInSamePrefecture(prefId int, begin stri
 	for rows.Next() {
 		common := Common{}
 		city := City{}
-		err = rows.Scan(&common.Id, &common.Year, &common.Month, &common.ResidentialUseTypeId, &common.ConstructionTypeId, &common.BuildTypeId, &common.ResidentialTypeId, &common.StructureType, &common.PrefId, &common.PrefName, &common.BuildDate, &city.CityId, &city.CityName, &city.BuiltCount, &city.TotalSquareMeter)
+		err = rows.Scan(&common.Id, &common.Year, &common.Month, &common.ResidentialUseTypeId, &common.ConstructionTypeId, &common.BuildTypeId, &common.ResidentialTypeId, &common.StructureType, &common.PrefId, &common.PrefName, &common.BuildDate, &city.CityId, &city.CityName, &city.BuiltCount, &city.TotalSquareMeter, &city.MonthlyRank)
 		res.Cities = append(res.Cities, city)
 		res.Common = common
 		if err != nil {
