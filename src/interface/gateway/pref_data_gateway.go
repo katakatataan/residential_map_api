db5ed0ee1ffbeb6fc3e7788ea3aa94dc7f9fbd31
katakatataan <yuki.matsuyama0123@gmail.com>
2018-12-31 16:35:58 +0900

    日付処理の暫定対応

--- src/interface/gateway/pref_data_gateway.go
+++ src/interface/gateway/pref_data_gateway.go
@@ -11,7 +11,7 @@ type PrefDataGateway struct {
 func (cdg *PrefDataGateway) FindByPrefId(prefId int, begin string, end string) (entity.PrefDatas, error) {
 	var prefDatas entity.PrefDatas
 	// TODO: マスターコード変換
-	err := cdg.Find(&prefDatas, "SELECT pref_id, pref_name, SUM(built_count) as built_count, SUM(total_square_meter) AS total_square_meter,to_char(build_date,'YYYY-MM') as build_date FROM city_data WHERE pref_id = $1 AND build_date >= $2 AND build_date < $3 GROUP BY pref_id, pref_name,  build_date ORDER BY  build_date ASC", prefId, begin, end)
+	err := cdg.Find(&prefDatas, "SELECT pref_id, pref_name, SUM(built_count) as built_count, SUM(total_square_meter) AS total_square_meter,to_char(build_date,'YYYY-MM') as build_date FROM city_data WHERE pref_id = $1 AND build_date >= $2 AND build_date < $3 GROUP BY pref_id, pref_name,  build_date ORDER BY  build_date ASC", prefId, string([]rune(begin)[:10]), string([]rune(end)[:10]))
 	if err != nil {
 		return entity.PrefDatas{}, err
 	}
