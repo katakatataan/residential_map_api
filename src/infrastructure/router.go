6bb5f5dbbbde2d9e09d8879313a2a5efc5f6bd37
katakatataan <yuki.matsuyama0123@gmail.com>
2019-01-13 17:16:38 +0900

    x-request-it

--- src/infrastructure/router.go
+++ src/infrastructure/router.go
@@ -6,6 +6,7 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/random"
 	_ "github.com/lib/pq"
 	"github.com/sevenNt/echo-pprof"
 )
@@ -18,8 +19,23 @@ type jwtCustomClaims struct {
 
 func Run(e *echo.Echo) {
 	// Middleware
-	e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `{"time":"${time_rfc3339_nano}","request_id":"${id}","remote_ip":"${remote_ip}","host":"${host}",` +
+			`"method":"${method}","uri":"${uri}","status":${status}, "latency":${latency},` +
+			`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
+			`"request_id":"${header:[X-Request-Id]}",` +
+			`"bytes_out":${bytes_out}}` + "\n",
+	}))
 	e.Use(middleware.Recover())
+	e.Use(middleware.RequestIDWithConfig(middleware.RequestIDConfig{
+		Generator: func() string {
+			requestId := e.AcquireContext().Request().Header.Get("X-Request-Id")
+			if requestId != "" {
+				requestId = random.String(32)
+			}
+			return requestId
+		},
+	}))
 	// Validator
 	e.Validator = NewValidator()
 
