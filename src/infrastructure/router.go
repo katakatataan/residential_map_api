a7280c573522879c2e0646ad511164a07d454113
katakatataan <yuki.matsuyama0123@gmail.com>
2019-01-14 01:03:44 +0900

    request idの取得とセット

--- src/infrastructure/router.go
+++ src/infrastructure/router.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/k0kubun/pp"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/labstack/gommon/random"
@@ -17,24 +18,29 @@ type jwtCustomClaims struct {
 	jwt.StandardClaims
 }
 
+// ServerHeader middleware adds a `Server` header to the response.
+func RequestId(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		requestId := c.Request().Header.Get("X-Request-Id")
+		pp.Println(requestId)
+		if requestId == "" {
+			requestId = random.String(32)
+		}
+		c.Response().Header().Set("X-Request-Id", requestId)
+		return next(c)
+	}
+}
+
 func Run(e *echo.Echo) {
 	// Middleware
+	e.Use(middleware.Recover())
+	e.Use(RequestId)
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `{"time":"${time_rfc3339_nano}","request_id":"${id}","remote_ip":"${remote_ip}","host":"${host}",` +
 			`"method":"${method}","uri":"${uri}","status":${status}, "latency":${latency},` +
 			`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
 			`"request_id":"${header:[X-Request-Id]}",` +
 			`"bytes_out":${bytes_out}}` + "\n",
-	}))
-	e.Use(middleware.Recover())
-	e.Use(middleware.RequestIDWithConfig(middleware.RequestIDConfig{
-		Generator: func() string {
-			requestId := e.AcquireContext().Request().Header.Get("X-Request-Id")
-			if requestId != "" {
-				requestId = random.String(32)
-			}
-			return requestId
-		},
 	}))
 	// Validator
 	e.Validator = NewValidator()
